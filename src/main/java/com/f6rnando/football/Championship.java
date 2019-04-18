ded8700edacded168675e4dd73af522d0ae59c24
Pinto, Fernando <fernando.pinto@thermofisher.com>
2019-04-18 13:27:46 -0700

    Championship game finished

--- src/main/java/com/f6rnando/football/Championship.java
+++ src/main/java/com/f6rnando/football/Championship.java
@@ -3,6 +3,8 @@ package com.f6rnando.football;
 public class Championship {
 
     private String[] scoreTable;
+    private int winnerScore;
+    private int looserScore;
 
     public void setScoreTable(String[] scoreTable) {
         this.scoreTable = scoreTable;
@@ -14,5 +16,52 @@ public class Championship {
 
     public String getMatch(int matchNumber) {
         return scoreTable[matchNumber];
+    }
+
+    public int getWinnerScore() {
+        return winnerScore;
+    }
+
+    public int getLooserScore() {
+        return looserScore;
+    }
+
+    public void calculateMatchScore() {
+        int teamA = 0;
+        int teamB = 0;
+        String[] matchScore;
+
+        for (String score : scoreTable) {
+            matchScore = score.split(":");
+
+            if (Integer.parseInt(matchScore[0]) > Integer.parseInt(matchScore[1])) {
+                // Team A won the match
+                teamA += 3;
+            } else if (Integer.parseInt(matchScore[1]) > Integer.parseInt(matchScore[0])) {
+                // Team B won the match
+                teamB += 3;
+            } else {
+                // It was a matched game
+                if (Integer.parseInt(matchScore[0]) > 0) {
+                    // Goals were scored
+                    teamA += 1;
+                    teamB += 1;
+                }
+            }
+        }
+
+        if (teamA > teamB) {
+            this.winnerScore = teamA;
+            this.looserScore = teamB;
+        } else if (teamA < teamB) {
+            this.winnerScore = teamB;
+            this.looserScore = teamA;
+        } else {
+            if (teamA > 0) {
+                this.winnerScore = this.looserScore = 10;
+            } else {
+                this.winnerScore = this.looserScore = 0;
+            }
+        }
     }
 }
--- src/test/java/com/f6rnando/football/FootballScoreTest.java
+++ src/test/java/com/f6rnando/football/FootballScoreTest.java
@@ -3,16 +3,19 @@ package com.f6rnando.football;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class FootballScoreTest {
     //-- A championship must have 10 matches
-    // A match must have a score for each team
-    // An undefeated team has a score of 30 points
-    // A team that never won has a score of 0 points
-    // A matched championship has a score of 10 points
-    // A winner team score 3 points
-    // A looser team score 0 points
-    // A matched game score 1 point
+    //-- A match must have a score for each team
+    //-- An undefeated team has a score of 30 points
+    //-- A team that never won has a score of 0 points
+    //-- A matched championship has a score of 10 points
+
+    // Rules
+    // 1. A team who win gets 3 points
+    // 2. With a matched game both teams get 1 point
+    // 3. If no goals are scored, both teams get 0 points
 
     @Test
     public void aFinishedChampionshipHas10Matches() {
@@ -60,5 +63,99 @@ public class FootballScoreTest {
 
         // Assert
         assertEquals(expectedQuantity, scoreQuantity);
+    }
+
+    @Test
+    public void anUndefeatedTeamHasAScoreOf30Points() {
+        // Setup
+        String[] scoreTable = {"2:0", "1:0", "3:1", "4:2", "3:2", "4:2", "5:4", "4:0", "5:4", "5:0"};
+        Championship championship = new Championship();
+
+        // Act
+        int expectedWinnerScore = 30;
+        championship.setScoreTable(scoreTable);
+        championship.calculateMatchScore();
+
+        // Assert
+        assertEquals(expectedWinnerScore, championship.getWinnerScore());
+    }
+
+    @Test
+    public void aTeamWhoNeverWonHasAScoreOfZero() {
+        // Setup
+        String[] scoreTable = {"2:0", "1:0", "3:1", "4:2", "3:2", "4:2", "5:4", "4:0", "5:4", "5:0"};
+        Championship championship = new Championship();
+
+        // Act
+        int expectedLooserScore = 0;
+        championship.setScoreTable(scoreTable);
+        championship.calculateMatchScore();
+
+        // Assert
+        assertEquals(expectedLooserScore, championship.getLooserScore());
+    }
+
+    @Test
+    public void aMatchedChampionshipHasAScoreOf10ForEachTeam() {
+        // Setup
+        String[] scoreTable = {"2:0", "1:0", "3:1", "4:2", "3:2", "0:2", "0:4", "0:4", "0:4", "0:3"};
+        Championship championship = new Championship();
+
+        // Act
+        int teamAScore = 10;
+        int teamBScore = 10;
+        championship.setScoreTable(scoreTable);
+        championship.calculateMatchScore();
+
+        // Assert
+        assertEquals(teamAScore, championship.getWinnerScore());
+        assertEquals(teamBScore, championship.getLooserScore());
+    }
+
+    @Test
+    public void aTeamWhoWon6TimesHasAScoreOf18() {
+        // Setup
+        String[] scoreTable = {"2:0", "1:0", "3:1", "4:2", "3:2", "4:2", "2:4", "0:0", "1:4", "0:0"};
+        Championship championship = new Championship();
+
+        // Act
+        int expectedWinnerScore = 18;
+        championship.setScoreTable(scoreTable);
+        championship.calculateMatchScore();
+
+        // Assert
+        assertEquals(expectedWinnerScore, championship.getWinnerScore());
+    }
+
+    @Test
+    public void aTeamWhoLost6TimesHasAScoreOf12() {
+        // Setup
+        String[] scoreTable = {"2:0", "1:0", "3:1", "4:2", "3:2", "4:2", "2:4", "0:1", "1:4", "0:1"};
+        Championship championship = new Championship();
+
+        // Act
+        int expectedLooserScore = 12;
+        championship.setScoreTable(scoreTable);
+        championship.calculateMatchScore();
+
+        // Assert
+        assertEquals(expectedLooserScore, championship.getLooserScore());
+    }
+
+    @Test
+    public void aChampionshipWithNoMatchesWonHasAScoreOfZeroForEachTeam() {
+        // Setup
+        String[] scoreTable = {"0:0", "0:0", "0:0", "0:0", "0:0", "0:0", "0:0", "0:0", "0:0", "0:0"};
+        Championship championship = new Championship();
+
+        // Act
+        int teamAScore = 0;
+        int teamBScore = 0;
+        championship.setScoreTable(scoreTable);
+        championship.calculateMatchScore();
+
+        // Assert
+        assertEquals(teamAScore, championship.getWinnerScore());
+        assertEquals(teamBScore, championship.getLooserScore());
     }
 }
